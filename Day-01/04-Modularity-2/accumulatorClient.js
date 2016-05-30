f3f678bba28718fc43291432b983ac461f88401e
Magesh <tkmagesh77@gmail.com>
2016-05-30 14:06:24 +0530

    accumulator assignment

---
+++ Day-01/04-Modularity-2/accumulatorClient.js
@@ -0,0 +1,9 @@
+//create an accumulator object
+
+var accumulator = require('./accumulator.js');
+
+accumulator.add(100);
+accumulator.subtract(50);
+accumulator.multiply(10);
+accumulator.divide(2);
+console.log(accumulator.getResult()); //=> 250
